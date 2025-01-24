4a1ade2ba62e44f6febc31f8f1fb9f71bc6d0443
Ashutosh Singh <133004845+Ashu117243@users.noreply.github.com>
2025-01-24 00:08:47 +0000

    datatypes in js

---
+++ 02-datatypes/datatype.js
@@ -0,0 +1,9 @@
+"use strict";
+// we are using name node js not browser
+//alert("why the fuk you did this ")
+
+// number max 2 ki power 53
+// null => standalone value (represntation of a value)
+// undefined = not defined yet 
+// symblo = unique
+console.log(typeof null)
